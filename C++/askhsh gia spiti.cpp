3ede13745bfcf6bb01c911288afce7ae07c4b35d
cntoumas <cntoumas@physics.auth.gr>
2024-12-11 09:58:29 +0200

    Update askhsh gia spiti.cpp

--- C++/askhsh gia spiti.cpp
+++ C++/askhsh gia spiti.cpp
@@ -1,10 +1,1 @@
-/* 
-Struct gia migadikous arithmous (pragmatiko kai fantastiko meros)
 
-function dexetai 2 migadikous kai gyrnaei athrisma kai diafora
-
-function gia eisodo twn metavlhtwn
-
-oi arithmoi einai pinakes 
-
-*/
