1e8a42fab27c5d0de4d564e9103ddf31a6db6c6c
cntoumas <cntoumas@physics.auth.gr>
2024-12-11 10:41:04 +0200

    Create animals.cpp

---
+++ Class exercises/Classes/animals.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+class Animals {
+    public:
+        int legs = 4;
+};
+
+class Dog : public Animals {
+    public: 
+        int tail = 1;
+};
+
+int main () {
+    Dog d;
+    cout << "The legs are: " << d.legs << endl;
+    cout << "The tail is: " << d.tail;
+    return 0 
+}
